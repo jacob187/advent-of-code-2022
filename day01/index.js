fadb5b875db630fb94dd685c17d4c6b1c7abf5c7
Jacob Cohen <79770350+jacob187@users.noreply.github.com>
2023-01-06 17:06:03 -0500

    Cleaner Solution Day 1

--- day01/index.js
+++ day01/index.js
@@ -1,24 +1,35 @@
 const fs = require("fs");
 const input = fs.readFileSync("./input.txt", "utf-8");
-const caloriesOfEleves = input.split("\n");
+const caloriesOfEleves = input.split("\n\n");
 
-//Part 1
-// loop through, create new array of sum of each elf, parse int , sort that array, return the first value
-let sumElfCalories = [];
-let elfSum = 0;
-caloriesOfEleves.forEach((i) => {
-  if (i == "") {
-    sumElfCalories.push(elfSum);
-    elfSum = 0;
-  } else {
-    elfSum += parseInt(i);
-  }
+//Part One Solution One
+const solutionOneProblemOne = (calList) => {
+  let sumElfCalories = [];
+  let elfSum = 0;
+  caloriesOfEleves.forEach((i) => {
+    if (i == "") {
+      sumElfCalories.push(elfSum);
+      elfSum = 0;
+    } else {
+      elfSum += parseInt(i);
+    }
+  });
+  sumElfCalories.sort((a, b) => b - a);
+
+  return sumElfCalories;
+};
+
+//Part One Solution Two
+const sumElfCalories = caloriesOfEleves.map((elfTotalCalories) => {
+  return elfTotalCalories
+    .split("\n")
+    .map(Number)
+    .reduce((bag, calories) => bag + calories);
 });
+
 sumElfCalories.sort((a, b) => b - a);
+
 console.log(sumElfCalories[0]);
 
 //Part 2
-
 console.log(sumElfCalories[0] + sumElfCalories[1] + sumElfCalories[2]);
-
-//TODO: solve using built in array functions (i.e. sort, reduce)
